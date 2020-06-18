142775d95ccc0f8f53a61ec192394e0b2fa8ec9d
Firman <fblazt@outlook.com>
2020-06-18 14:07:50 +0700

    Setting up front-end structure

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import Router from 'vue-router';
+import VueAxios from 'vue-axios';
+import Axios from 'axios';
+
+Vue.use(Router,VueAxios,Axios);
+
+import App from './components/App.vue';
+import Home from './components/Home.vue';
+
+const routes = [
+    {
+    path: '/',
+    name: 'home',
+    component: Home
+    }
+]
+
+const router = new Router({ mode: 'history', routes: routes });
+new Vue(Vue.util.extend({ router }, App)).$mount("#app");

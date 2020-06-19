d5f3b552080083d4ae523151dc94ff6d8acefc3c
Firman <fblazt@outlook.com>
2020-06-20 06:23:16 +0700

    Add bottom section of footer component

--- resources/js/components/Footer/Footer.vue
+++ resources/js/components/Footer/Footer.vue
@@ -1,16 +1,19 @@
 <template>
   <footer class="w-full pt-12 footer-bg">
     <FooterTop></FooterTop>
+    <FooterBottom></FooterBottom>
   </footer>
 </template>
 
 <script>
 import FooterTop from './FooterTop'
+import FooterBottom from './FooterBottom'
 
 export default {
   name: 'Footer',
   components: {
-    FooterTop
+    FooterTop,
+    FooterBottom
   }
 }
 </script>
---
+++ resources/js/components/Footer/FooterBottom.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container mx-auto p-2">
+    <div class="flex flex-row flex-wrap justify-between text-sm text-gray-300">
+      <div>
+        <p>Copyright ©2016 Niagahoster | Hosting powered by PHP7, CloudLinux, CloudFlare, BitNinja and DC Biznet Technovillage Jakarta</p>
+        <p>Cloud VPS Murah powered by Webuzo Softaculous, Intel SSD and cloud computing technology</p>
+      </div>
+      <p class="mt-1 md:mt-0">
+        <a href="#" class="hover:underline">Syarat dan Ketentuan</a> | 
+        <a href="#" class="hover:underline">Kebijakan Privasi</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterBottom'
+}
+</script>
+
+<style>
+
+</style>
